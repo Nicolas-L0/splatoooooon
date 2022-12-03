8f42876b3dd10b4a832fa2a6231b68b81bb2dd7f
nicL <niceugeo@outlook.com>
2022-12-03 17:31:45 +0800

    [mpm] complete p2g

--- splatoooooon.py
+++ splatoooooon.py
@@ -2,34 +2,65 @@ import taichi as ti
 
 ti.init(arch=ti.gpu)
 
-# a = float('inf')
-# b = float('-inf')
+# constant
+steps = 25
+dt = 2e-4
+GRAVITY = -9.8
+gravity = ti.Vector.field(3, dtype=float, shape=())
+gravity[None] = [0, GRAVITY, 0]
+E = 1000
 
-mouse_circle = ti.Vector.field(2, dtype=float, shape=(1, ))
+# grid properties
+n_grids = 64
+g_v = ti.Vector.field(3, float, (n_grids, n_grids, n_grids))
+g_m = ti.field(float, (n_grids, n_grids, n_grids))
 
+dx = 1 / n_grids
+
+# particle properties
+n_particles = 2**14
+p_rho = 1
+p_vol = (dx * 0.5)**2
+p_mass = p_vol * p_rho
+p_x = ti.Vector.field(3, float, n_particles)
+p_v = ti.Vector.field(3, float, n_particles)
+p_acc = ti.Vector.field(3, float, shape=())
+p_colors = ti.Vector.field(4, float, n_particles)
+p_C = ti.Matrix.field(3, 3, float, n_particles)
+p_J = ti.field(float, n_particles)
+#p_used = ti.field(int, n_particles)
+
+@ti.kernel
+def substep():
+    # init grid
+    for i, j, k in g_m:
+        g_v[i, j, k] = [0, 0, 0]
+        g_m[i, j, k] = 0
+    # P2G
+    for p in p_x:
+        Xp = p_x[p] / dx
+        base = int(Xp - 0.5)  #floor(Xp - 0.5)
+        fx = Xp - base
+        # Quadratic B-spline
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
+        stress = -dt * 4 * E * p_vol * (p_J[p] - 1) / dx**2  #internal force [MSLMPM]
+        affine = ti.Matrix([[stress, 0], [0, stress]]) + p_mass * p_C[p]  #[MSLMPM]
+        # loop over 3*3*3 grid node neighbor
+        for offset in ti.static(ti.ndrange(3,3,3)):
+            dpos = (offset - fx) * dx
+            weight = 1.0
+            # calculate x y z component for every neighbor grid 
+            for i in ti.static(range(3)):
+                weight *= w[offset[i]][i]  #???WHY
+                g_v[base + offset] += weight * (p_mass * p_v[p] + affine @ dpos)  #Grid momentum / mass [MSLMPM]
+                g_m[base + offset] += weight * p_mass   #Grid mass
+    # Grid operations
+    for i, j, k in g_m:
+
+
+    
 def main():
-    print(
-        "splatoooooon!"
-    )
-
-    res = (1080, 720)
-    window = ti.ui.Window("splatoooooon", res, vsync=True)
-    canvas = window.get_canvas()
-    global radius
-    radius = 0.003
-
-    # GGUI
-    while window.running:
-        if window.get_event(ti.ui.PRESS):
-            if window.event.key in [ti.ui.ESCAPE]:
-                break
-        mouse = window.get_cursor_pos()
-        mouse_circle[0] = ti.Vector([mouse[0], mouse[1]])
-        canvas.circles(mouse_circle, color=(0.2, 0.4, 0.6), radius=0.01)
-        if window.is_pressed(ti.ui.LMB):
-            canvas.circles(mouse_circle, color=(0.8, 0.1, 0.1), radius=0.05)
-        canvas.set_background_color((0.5, 0.5, 0.5))
-        window.show()
+    substep()
 
 if __name__ == '__main__':
     main()
